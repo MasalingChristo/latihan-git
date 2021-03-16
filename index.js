4ab0a32b5a23a3608882daac9f7c0ce37e554aca
MasalingChristo <s11710064@student.unklab.ac.id>
2021-03-15 20:24:41 -0800

    menambahkan file pertemuan 3

---
+++ index.js
@@ -0,0 +1,53 @@
+console.log("Hallo Whord!!!");
+
+//1. Variable
+//var nama="Christo";
+
+let namaDepan="Christo";
+let namaBelakang='Masaling'
+
+nama = 'Nando'
+
+let usia = 24
+
+usia = "dua puluh empat Tahun"
+let apakahSudahMenikah =true
+
+
+console.log("Hallo Nama Saya" + " " + namaDepan + " " + namaBelakang + ".Umur saya adalah" + usia )
+console.log(usia)
+
+console.log(apakahSudahMenikah)
+
+
+
+//2. Oprator 
+
+
+
+// console.log(10 == 10)// loose equality
+// console.log(10 === "10")// strict equality
+// console.log(10 !==10)
+
+// //Ternary Operator
+
+// const Hasil = 10 % 2 ===0 ? "Genap " : "Ganjil"
+
+// console.log(Hasil)
+
+//Function
+//Function declaration
+// function ucapkanSalam(){
+//     return "Selamat Sore"
+// }
+
+// console.log(ucapkanSalam())
+
+// //Function expretion
+// const ucapkanSalam2 = function(){
+//     return "Selamat siang"
+// }
+
+// console.log(ucapkanSalam2())
+
+
